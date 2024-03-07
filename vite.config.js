499a5afb360bd43cd5baae6f267aa2c1a0e3ce98
Antonio Manno <antonio.manno.7@gmail.com>
2024-03-07 21:18:02 +0000

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./got-character-gallery/src/tests/setup.js",
+    setupFiles: "./src/tests/setup.js",
   },
 });
